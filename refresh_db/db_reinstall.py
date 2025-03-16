70673e1692ad3efdae616c5c84d559311a69bf5e
Xavier Martínez <xavier1997@gmail.com>
2025-03-16 12:25:26 +0100

    Bring latest changes from master.

--- refresh_db/db_reinstall.py
+++ refresh_db/db_reinstall.py
@@ -1,5 +1,5 @@
 import datetime
-import json
+import glob
 import os
 import time
 from pathlib import Path
@@ -37,6 +37,15 @@ MYSQL_IGNORED_EXCEPTIONS = {
 target_db = Database('LOCAL')
 origin_db = Database('PROD')
 
+def get_newest_file(directory):
+    files = glob.glob(os.path.join(directory, '*'))  # Get all files in directory
+    if not files:  # If no files found, return None
+        return None
+    newest_file = max(files, key=os.path.getmtime)  # Get file with latest modification time
+    return newest_file
+
+def get_file_size_in_mb(file_path):
+    return os.path.getsize(file_path) / (1024 * 1024)
 
 def execute_scripts_from_file(filename, error_color=None):
     cnx = mysql_connector.connect(user=target_db.user, password=target_db.passwd, host=target_db.host,
@@ -70,11 +79,14 @@ def execute_scripts_from_file(filename, error_color=None):
 
 
 def generate_production_dump():
-    # toDO: dynamic size
     if not os.path.exists(SQL_DUMPS):
         os.makedirs(SQL_DUMPS)
-    print("Today's dump not found, downloading...")
-    os.system(f"mysqldump --single-transaction -h {origin_db.host} -u {origin_db.user} -p{origin_db.passwd} {origin_db.database}{' | pv --size 56m' if os.name == 'posix' else ''} > dumps/{datetime.datetime.today().date().isoformat()}-dump.sql")
+        last_dump_mb = 1000
+    else:
+        last_dump_mb = int(get_file_size_in_mb(get_newest_file(SQL_DUMPS)))
+    print(f"Today's dump not found, downloading from {origin_db.host}...")
+    posix_pv_size_string = f" | pv --size {last_dump_mb}m"
+    os.system(f"mysqldump --single-transaction --column-statistics=0 -h {origin_db.host} -u {origin_db.user} -p{origin_db.passwd} {origin_db.database}{posix_pv_size_string if os.name == 'posix' else ''} > dumps/{datetime.datetime.today().date().isoformat()}-dump.sql")
 
 
 def get_prod_dump():
@@ -120,9 +132,11 @@ def main():
     else:
         raise Exception("Mac not supported :P")
 
+    dump = get_prod_dump()
 
     if RESET_DB:
         # Setup
+        input(f"Confirm drop of {target_db.host}")
         print(f"Dropping old database {target_db.database}...")
         os.system(f"{sql_cl} -e {quote}DROP DATABASE {target_db.database}{quote}")
 
@@ -130,7 +144,6 @@ def main():
         os.system(f"{sql_cl} -e {quote}CREATE DATABASE {target_db.database}{quote}")
 
         # print("Importing dump into database...")
-        dump = get_prod_dump()
         print("Importing dump: ", colored(dump.name, "green"))
         if os.name == 'nt':
             os.system(f"{sql_cl_db} < {dump.absolute()}")
@@ -155,3 +168,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # get_prod_dump()
